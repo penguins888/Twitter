972878324f0c2a00622edf4b224398ce8bc0976f
penguins888 <57269064+penguins888@users.noreply.github.com>
2020-03-11 18:03:31 -0700

    Add files via upload

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,63 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Michelle Lu on 3/8/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var characters: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
+        
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+        if(!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+                
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let characterLimit = 140
+        
+        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+        let newTextCount = newText.count
+        
+        characters.text = String(characterLimit - newTextCount) + " out of 140"
+        
+        return newTextCount <  characterLimit
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
